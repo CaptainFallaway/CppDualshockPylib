6c30359769e443aefdafd2b059cff5dc65491511
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-01-18 00:15:35 +0100

    Alot done, forgot to commit

--- include/dualshockinterface.h
+++ include/dualshockinterface.h
@@ -2,7 +2,6 @@
 #include <mutex>
 #include <fstream>
 
-
 struct EventData {
     long timestamp;
     long timestamp_decimal;
@@ -11,10 +10,9 @@ struct EventData {
     int value;
 };
 
-
-class EventDataHandler {
+class EventDataRegistrar {
     public:
-        EventDataHandler();
+        EventDataRegistrar();
         void set(EventData& eventData);
         EventData get();
     private:
@@ -22,47 +20,47 @@ class EventDataHandler {
         std::mutex mutex;
 };
 
-
 class DualshockInterface {
 public:
     // Constructor and destructor
-    DualshockInterface(const std::string& eventStreamPath);
+    DualshockInterface(const std::string& eventStreamPath) : eventStreamPath(eventStreamPath) {
+        stopRequested = false;
+    
+        btnCross.set(EventData());
+    };
 
     ~DualshockInterface();
 
-    // Methods to get the "newest" button events from the EventData stream
+    // EventData data handlers
 
-    EventData getBtnCross();
-    EventData getBtnCircle();
-    EventData getBtnSquare();
-    EventData getBtnTriangle();
-    EventData getBtnL1();
-    EventData getBtnL2();
-    EventData getBtnL3();
-    EventData getBtnR1();
-    EventData getBtnR2();
-    EventData getBtnR3();
-    EventData getBtnShare();
-    EventData getBtnOptions();
-    EventData getBtnPS();
+    EventDataRegistrar btnCross;
+    EventDataRegistrar btnCircle;
+    EventDataRegistrar btnSquare;
+    EventDataRegistrar btnTriangle;
+    EventDataRegistrar btnL1;
+    EventDataRegistrar btnL2;
+    EventDataRegistrar btnL3;
+    EventDataRegistrar btnR1;
+    EventDataRegistrar btnR2;
+    EventDataRegistrar btnR3;
+    EventDataRegistrar btnShare;
+    EventDataRegistrar btnOptions;
+    EventDataRegistrar btnPS;
 
-    // Methods to get the "newest" axis events from the EventData stream
-
-    EventData getAxisLeftStickX();
-    EventData getAxisLeftStickY();
-    EventData getAxisRightStickX();
-    EventData getAxisRightStickY();
-    EventData getAxisL2();
-    EventData getAxisR2();
-    EventData getAxisDPadX();
-    EventData getAxisDPadY();
+    EventDataRegistrar axisLeftStickX;
+    EventDataRegistrar axisLeftStickY;
+    EventDataRegistrar axisRightStickX;
+    EventDataRegistrar axisRightStickY;
+    EventDataRegistrar axisL2;
+    EventDataRegistrar axisR2;
+    EventDataRegistrar axisDPadX;
+    EventDataRegistrar axisDPadY;
 
     // Method to start the C++ loop in a separate thread
     void startListening();
 
     // Method to stop the C++ loop
     void stop();
-
 private:
     // EventData stream path
     const std::string eventStreamPath;
@@ -73,31 +71,6 @@ private:
     std::thread loopThread;  // Thread for the C++ loop
     std::mutex dataMutex;    // Mutex for synchronizing data access
     bool stopRequested;      // Flag to signal the loop to stop
-
-    // EventData data handlers
-
-    EventDataHandler btnCross;
-    EventDataHandler btnCircle;
-    EventDataHandler btnSquare;
-    EventDataHandler btnTriangle;
-    EventDataHandler btnL1;
-    EventDataHandler btnL2;
-    EventDataHandler btnL3;
-    EventDataHandler btnR1;
-    EventDataHandler btnR2;
-    EventDataHandler btnR3;
-    EventDataHandler btnShare;
-    EventDataHandler btnOptions;
-    EventDataHandler btnPS;
-
-    EventDataHandler axisLeftStickX;
-    EventDataHandler axisLeftStickY;
-    EventDataHandler axisRightStickX;
-    EventDataHandler axisRightStickY;
-    EventDataHandler axisL2;
-    EventDataHandler axisR2;
-    EventDataHandler axisDPadX;
-    EventDataHandler axisDPadY;
 
     // Method for the C++ loop
     void loop();
--- src/dualshockinterface.cpp
+++ src/dualshockinterface.cpp
@@ -1,55 +1,49 @@
 #include "dualshockinterface.h"
+#include "constants.h"
 
+// Initialize the EventDataRegistrar with an empty EventData struct and a mutex used for thread safety when setting and getting the data
+EventDataRegistrar::EventDataRegistrar() : eventData(), mutex() {}
 
-EventDataHandler::EventDataHandler() : eventData(), mutex() {}
-
-
-void EventDataHandler::set(EventData& eventData) {
+void EventDataRegistrar::set(EventData& eventData) {
     std::lock_guard<std::mutex> lock(mutex);
     this->eventData = eventData;
 }
 
-
-EventData EventDataHandler::get() {
+EventData EventDataRegistrar::get() {
     std::lock_guard<std::mutex> lock(mutex);
     return eventData;
 }
 
-
+// Initialize the DualshockInterface
 DualshockInterface::DualshockInterface(const std::string& eventStreamPath) : eventStreamPath(eventStreamPath), stopRequested(false) {};
 
-
 DualshockInterface::~DualshockInterface() {
-    // Stop the loop if it is running
-    if (loopThread.joinable()) {
-        stop();
-    }
+    stop();
 }
 
-
 void DualshockInterface::startListening() {
-    eventStream.open(eventStreamPath, std::ios::in | std::ios::binary);
-
-    // Start the C++ loop in a separate thread
     loopThread = std::thread(&DualshockInterface::loop, this);
 }
 
-
 void DualshockInterface::stop() {
-    // Signal the loop to stop
     stopRequested = true;
+    eventStream.close();
 
-    // Wait for the loop to finish
-    loopThread.join();
+    if (loopThread.joinable()) 
+        loopThread.join();
 }
 
-
 void DualshockInterface::loop() {
-    if (!eventStream.is_open()) {
-        throw std::runtime_error("Event stream not open");
+    // The loop that runs in a separate thread
+
+    eventStream.open(eventStreamPath, std::ios::in | std::ios::binary);  // Open the event stream
+
+    if (!eventStream.is_open() || eventStream.fail()) {
+        throw std::runtime_error("Event stream failed to open!\nSee if controller has disconnected or changed event stream path.");
         return;
     }
 
+    // Initialize a EventData so it can be mutated in the loop
     EventData event;
 
     // Loop until stop is requested
@@ -58,10 +52,10 @@ void DualshockInterface::loop() {
 
         switch (event.type)
         {
-            case (1):
+            case (EV_KEY):
                 handleKeyEvent(event);
                 break;
-            case (3):
+            case (EV_ABS):
                 handleAxisEvent(event);
                 break;
             default:
@@ -70,32 +64,78 @@ void DualshockInterface::loop() {
     }
 }
 
+void DualshockInterface::handleKeyEvent(EventData& event) {
+    switch (event.code)
+    {
+        case (BTN_CROSS):
+            btnCross.set(event);
+            break;
+        case (BTN_CIRCLE):
+            btnCircle.set(event);
+            break;
+        case (BTN_TRIANGLE):
+            btnTriangle.set(event);
+            break;
+        case (BTN_SQUARE):
+            btnSquare.set(event);
+            break;
+        case (BTN_L1):
+            btnL1.set(event);
+            break;
+        case (BTN_R1):
+            btnR1.set(event);
+            break;
+        case (BTN_L2):
+            btnL2.set(event);
+            break;
+        case (BTN_R2):
+            btnR2.set(event);
+            break;
+        case (BTN_SHARE):
+            btnShare.set(event);
+            break;
+        case (BTN_OPTIONS):
+            btnOptions.set(event);
+            break;
+        case (BTN_PS):
+            btnPS.set(event);
+            break;
+        case (BTN_L3):
+            btnL3.set(event);
+            break;
+        case (BTN_R3):
+            btnR3.set(event);
+            break;
+        default:
+            break;
+    }
+}
 
 void DualshockInterface::handleAxisEvent(EventData& event) {
     switch (event.code)
     {
-        case (0):
+        case (ABS_X):
             axisLeftStickX.set(event);
             break;
-        case (1):
+        case (ABS_Y):
             axisLeftStickY.set(event);
             break;
-        case (2):
+        case (ABS_Z):
             axisL2.set(event);
             break;
-        case (3):
+        case (ABS_RX):
             axisRightStickX.set(event);
             break;
-        case (4):
+        case (ABS_RY):
             axisRightStickY.set(event);
             break;
-        case (5):
+        case (ABS_RZ):
             axisR2.set(event);
             break;
-        case (16):
+        case (ABS_HAT0X):
             axisDPadX.set(event);
             break;
-        case (17):
+        case (ABS_HAT0Y):
             axisDPadY.set(event);
             break;
         default:
@@ -104,49 +144,109 @@ void DualshockInterface::handleAxisEvent(EventData& event) {
 }
 
 
-void DualshockInterface::handleKeyEvent(EventData& event) {
-    switch (event.code)
-    {
-        case (304):
-            btnCross.set(event);
-            break;
-        case (305):
-            btnCircle.set(event);
-            break;
-        case (307):
-            btnTriangle.set(event);
-            break;
-        case (308):
-            btnSquare.set(event);
-            break;
-        case (310):
-            btnL1.set(event);
-            break;
-        case (311):
-            btnR1.set(event);
-            break;
-        case (312):
-            btnL2.set(event);
-            break;
-        case (313):
-            btnR2.set(event);
-            break;
-        case (314):
-            btnShare.set(event);
-            break;
-        case (315):
-            btnOptions.set(event);
-            break;
-        case (316):
-            btnPS.set(event);
-            break;
-        case (317):
-            btnL3.set(event);
-            break;
-        case (318):
-            btnR3.set(event);
-            break;
-        default:
-            break;
+// C interface for the DualshockInterface class to be used in Python with ctypes
+extern "C" {
+    DualshockInterface* DualshockInterface_new(const char* eventStreamPath) {
+        return new DualshockInterface(eventStreamPath);
+    }
+
+    void DualshockInterface_delete(DualshockInterface* dualshockInterface) {
+        delete dualshockInterface;
+    }
+
+    void DualshockInterface_startListening(DualshockInterface* dualshockInterface) {
+        dualshockInterface->startListening();
+    }
+
+    void DualshockInterface_stop(DualshockInterface* dualshockInterface) {
+        dualshockInterface->stop();
+    }
+
+    EventData DualshockInterface_getBtnCross(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnCross.get();
+    }
+
+    EventData DualshockInterface_getBtnCircle(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnCircle.get();
+    }
+
+    EventData DualshockInterface_getBtnSquare(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnSquare.get();
+    }
+
+    EventData DualshockInterface_getBtnTriangle(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnTriangle.get();
+    }
+
+    EventData DualshockInterface_getBtnL1(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnL1.get();
+    }
+
+    EventData DualshockInterface_getBtnL2(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnL2.get();
+    }
+
+    EventData DualshockInterface_getBtnL3(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnL3.get();
+    }
+
+    EventData DualshockInterface_getBtnR1(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnR1.get();
+    }
+
+    EventData DualshockInterface_getBtnR2(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnR2.get();
+    }
+
+    EventData DualshockInterface_getBtnR3(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnR3.get();
+    }
+
+    EventData DualshockInterface_getBtnShare(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnShare.get();
+    }
+
+    EventData DualshockInterface_getBtnOptions(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnOptions.get();
+    }
+
+    EventData DualshockInterface_getBtnPS(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->btnPS.get();
+    }
+
+    EventData DualshockInterface_getAxisLeftStickX(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisLeftStickX.get();
+    }
+
+    EventData DualshockInterface_getAxisLeftStickY(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisLeftStickY.get();
+    }
+
+    EventData DualshockInterface_getAxisRightStickX(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisRightStickX.get();
+    }
+
+    EventData DualshockInterface_getAxisRightStickY(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisRightStickY.get();
+    }
+
+    EventData DualshockInterface_getAxisL2(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisL2.get();
+    }
+
+    EventData DualshockInterface_getAxisR2(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisR2.get();
+    }
+
+    EventData DualshockInterface_getAxisDPadX(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisDPadX.get();
+    }
+
+    EventData DualshockInterface_getAxisDPadY(DualshockInterface* dualshockInterface) {
+        return dualshockInterface->axisDPadY.get();
+    }
+
+    void DualshockInterface_delete (DualshockInterface* dualshockInterface) {
+        delete dualshockInterface;
     }
 }
